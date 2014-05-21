ca8c4dd69aeb181c4421a231f80b0453931671bd
Byakuya <inf.byakuya@gmail.com>
2014-05-21 02:21:35 +0200

    Minor fixes summary.#

--- app/src/main/java/services/DataSaver.java
+++ app/src/main/java/services/DataSaver.java
@@ -6,6 +6,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import model.ElectricDevice;
 import model.RefuseProduction;
