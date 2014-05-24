ebd067a8efda7255b6db9b862a67c2668fcd63e7
Byakuya <inf.byakuya@gmail.com>
2014-05-24 07:01:44 +0200

    ShowUserActivity update.#

---
+++ app/src/main/java/greensaver/app/ExpandableGridView.java
@@ -0,0 +1,29 @@
+package greensaver.app;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+import android.widget.GridView;
+
+public class ExpandableGridView extends GridView {
+
+    public ExpandableGridView(Context context) {
+        super(context);
+    }
+
+    public ExpandableGridView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ExpandableGridView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int expandSpec = MeasureSpec.makeMeasureSpec(MEASURED_SIZE_MASK, MeasureSpec.AT_MOST);
+        super.onMeasure(widthMeasureSpec, expandSpec);
+        ViewGroup.LayoutParams params = getLayoutParams();
+        params.height = getMeasuredHeight();
+    }
+}
