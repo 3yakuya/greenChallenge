dd4e725df604cd22ca7d7f47ff9941276f48c99f
Byakuya <inf.byakuya@gmail.com>
2014-05-25 04:49:15 +0200

    Visual update.#

--- app/src/main/java/greensaver/app/ImageButtonAdapter.java
+++ app/src/main/java/greensaver/app/ImageButtonAdapter.java
@@ -1,5 +1,6 @@
 package greensaver.app;
 
+import android.app.ActionBar;
 import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,10 +23,8 @@ public class ImageButtonAdapter extends BaseAdapter{
 
         if (convertView == null) {
             imageButton = new ImageButton(this.context);
-            //String name = elements[position].getName();
-            //int drawableResourceId = context.getResources().getIdentifier(name, "drawable", context.getPackageName());
-            //Test stuff below:
-            int drawableResourceId = context.getResources().getIdentifier("ic_launcher", "drawable", context.getPackageName());
+            String name = this.recognizeName(elements[position].getName());
+            int drawableResourceId = context.getResources().getIdentifier(name, "drawable", context.getPackageName());
             imageButton.setImageResource(drawableResourceId);
         } else {
             imageButton = (ImageButton) convertView;
@@ -48,6 +47,31 @@ public class ImageButtonAdapter extends BaseAdapter{
     @Override
     public long getItemId(int position) {
         return position;
+    }
+
+    private String recognizeName(String name) {
+        char first = name.charAt(0);
+        switch (first) {
+            case 'T':
+                return "tv";
+            case 'R':
+                if (name.charAt(1) == 'a')
+                    return "radio";
+                else
+                    return "router";
+            case 'C':
+                return "computer";
+            case 'P':
+                return "printer";
+            case 'S':
+                return "set_top_box";
+            case 'D':
+                return "dvd_set";
+            case 'M':
+                return "microwave";
+            default:
+                return "ic_launcher";
+        }
     }
 
 }
--- app/src/main/java/greensaver/app/MainActivity.java
+++ app/src/main/java/greensaver/app/MainActivity.java
@@ -11,7 +11,10 @@ import android.view.MenuItem;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 
+import java.util.Arrays;
+
 import model.User;
+import model.UserStats;
 import services.DataLoader;
 import services.DataManager;
 import services.DataSaver;
@@ -19,25 +22,27 @@ import services.DataSaver;
 
 public class MainActivity extends Activity {
 
+    private String[] bckgrnd = {"0", "1", "2", "10", "20", "baa", "caa"};
+
+    private final int[] powerLimits = {6000, 3000};
+    private final int[] waterLimits = {160, 100};
+    private final int[] refuseLimits = {31, 14};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //////////////////////////////////////////TEST stuff /////////////////////////////
         RelativeLayout layout =(RelativeLayout) findViewById(R.id.background);
-        layout.setBackgroundResource(R.drawable.background_main);
-        ImageButton electricityImageButton = (ImageButton) findViewById(R.id.electricityImageButton);
-        ImageButton waterImageButton = (ImageButton) findViewById(R.id.waterImageButton);
-        ImageButton refuseImageButton = (ImageButton) findViewById(R.id.refuseImageButton);
-        electricityImageButton.setImageResource(R.drawable.ic_launcher);
-        waterImageButton.setImageResource(R.drawable.ic_launcher);
-        refuseImageButton.setImageResource(R.drawable.ic_launcher);
+        layout.setBackgroundResource(this.selectBackgroundImage());
         ActionBar actionBar = getActionBar();
         actionBar.show();
         DataManager dataManager = DataManager.getInstance();
         dataManager.storeElectricDeviceData("TV", 0, 0, 0, 0, 0);
-        dataManager.storeElectricDeviceData("Komp", 10, 100, 123, 32, 132);
-        dataManager.storeElectricDeviceData("Router", 10, 10, 10, 10, 10);
+        dataManager.storeElectricDeviceData("Computer", 10, 1000, 1230, 320, 132);
+        dataManager.storeElectricDeviceData("Router", 10, 1000, 10, 10, 10);
+        dataManager.storeElectricDeviceData("DVD set", 10, 1000, 1230, 320, 132);
+        dataManager.storeElectricDeviceData("Radio", 10, 1000, 1230, 320, 132);
         dataManager.storeWaterActivityData("Bath", 100, 1);
         dataManager.storeWaterActivityData("Washing Machine", 50, 1);
         dataManager.storeRefuseProductionData("Bags", 10);
@@ -45,6 +50,7 @@ public class MainActivity extends Activity {
         dataSaver.saveDataToFile(this);
         DataLoader dataLoader = DataLoader.getInstance();
         dataLoader.loadDataFromFile(this);
+        layout.setBackgroundResource(this.selectBackgroundImage());
     }
 
 
@@ -71,5 +77,58 @@ public class MainActivity extends Activity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    private int selectBackgroundImage() {
+        int currentScore = this.getCurrentUserScore();
+        switch (currentScore) {
+            case 0:
+                return R.drawable.aaa;
+            case 1:
+                return R.drawable.aab;
+            case 2:
+                return R.drawable.aac;
+            case 10:
+                return R.drawable.aba;
+            case 20:
+                return R.drawable.aca;
+            case 100:
+                return R.drawable.baa;
+            case 200:
+                return R.drawable.caa;
+            default:
+                return R.drawable.aaa;
+        }
+    }
+
+    private int getCurrentUserScore() {
+        DataManager dataManager = DataManager.getInstance();
+        dataManager.prepareUserStats();
+        UserStats userStats = UserStats.getInstance();
+        int score;
+        int electricPowerScore = userStats.getPowerUsage();
+        if (electricPowerScore > powerLimits[0]) {
+            score = 200;
+        } else if (electricPowerScore > powerLimits[1]) {
+            score = 100;
+        } else {
+            score = 0;
+        }
+
+        int waterScore = userStats.getWaterUsage();
+        if (waterScore > waterLimits[0]) {
+            score += 20;
+        } else if (waterScore > waterLimits[1]) {
+            score += 10;
+        }
+
+        int refuseScore = userStats.getRefuseProductionPoints();
+        if (refuseScore > refuseLimits[0]) {
+            score += 2;
+        } else if (refuseScore > refuseLimits[1]) {
+            score += 1;
+        }
+
+        return score;
     }
 }
