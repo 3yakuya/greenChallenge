5cd560991634d897f303ddd5b312c1ac368eeb66
Byakuya <inf.byakuya@gmail.com>
2014-09-18 18:10:58 +0200

    Added AddElementBasicActivity, to create an universal menu for setting basic element usage.#

--- app/src/main/java/greensaver/app/AddElementBasicActivity.java
+++ app/src/main/java/greensaver/app/AddElementBasicActivity.java
@@ -18,7 +18,7 @@ public class AddElementBasicActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.add_electric_device_basic, menu);
+        getMenuInflater().inflate(R.menu.add_element_basic, menu);
         return true;
     }
 
